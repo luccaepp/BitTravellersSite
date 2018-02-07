d5ebc33419b20a0649bae8cdd9c681eb7a154b95
Lucca <luccaepp@gmail.com>
2018-02-07 18:45:55 -0200

    site inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';     // Add this
+import { AboutComponent } from './about/about.component';  // Add this
+import { SobreComponent } from './sobre/sobre.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { ContatoComponent } from './contato/contato.component';
+
+
+const routes: Routes = [
+    {
+      path: 'home',
+      component: HomeComponent
+    },
+    {
+      path: 'projetos',
+      component: ProjetosComponent
+    },
+    {
+      path: 'sobre',
+      component: SobreComponent
+    },
+    {
+      path: 'contato',
+      component: ContatoComponent
+    }
+  ];
+
+  
+@NgModule({
+
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
+
+})
+
+export class AppRoutingModule{
+
+
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import  {AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DataService } from './data.service';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjetosComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
